036f66681fc80a35738b2821ff8100bee1ad2660
marwanabubakr <dev@marobakr>
2023-11-05 22:55:03 +0200

    checked

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-// Add Class Active For Link
-document.querySelectorAll('ul li a').forEach((link) => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault()
-    // Remove the 'active' class from all links
-    document.querySelectorAll('ul li a').forEach((link) => {
-      link.classList.remove('active_Anchor')
-    })
-    // Add the 'active' class to the clicked link
-    this.classList.add('active_Anchor')
-  })
-})
-// Add Border Bottom To Links
-document.querySelectorAll('ul li').forEach((link) => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault()
-    // Remove the 'active' class from all links
-    document.querySelectorAll('ul li ').forEach((link) => {
-      link.classList.remove('active_border')
-    })
-    // Add the 'active' class to the clicked link
-    this.classList.add('active_border')
-  })
-})
+// function Get Year of footer
+const currentDate = new Date().getFullYear()
+const yearElement = (document.getElementById(
+  'year'
+).innerHTML += `  ${currentDate} `)
